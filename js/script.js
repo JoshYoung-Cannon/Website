ce6b8c3aee136af3df28bf319130f2fcbc041eb4
Tola12343 <tolaainabadejo@yahoo.co.uk>
2020-02-28 14:04:32 +0000

    second commit

--- js/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('html').addEventListener('scroll', function () {
-    document.getElementById('head').style.transition = 'all 3s';
-    document.getElementById('head').style.display = 'none';
-});
